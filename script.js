c7df403660a85cab82df0580d9f770c4bfd26ba2
projectsofkhan <zeeshan40u@gmail.com>
2026-01-31 14:44:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,28 @@ const appGrid = document.getElementById("appGrid");
 const timeEl = document.getElementById("current-time");
 
 const apps = [
-  { name: "Messages", icon: "💬", file: "apps/messages/index.html" },
-  { name: "Phone", icon: "📞", file: "apps/phone/index.html" },
-  { name: "Gallery", icon: "🌄", file: "apps/gallery/index.html" },
-  { name: "Instashan", icon: "📸", file: "apps/instashan/index.html" },
-  { name: "Tasks", icon: "📋", file: "apps/task/index.html" },
-  { name: "Diary", icon: "📖", file: "apps/diary/index.html" },
-  { name: "Browser", icon: "🌐", file: "apps/browser/index.html" },
-  { name: "Settings", icon: "⚙️", file: "apps/settings/index.html" }
+  { name: "Messages", icon: "💬", color: "#579AD9", file: "apps/messages/index.html" },
+  { name: "Phone", icon: "📞", color: "#6BBF6B", file: "apps/phone/index.html" },
+  { name: "Gallery", icon: "🌄", color: "#6A618F", file: "apps/gallery/index.html" },
+  { name: "Instashan", icon: "📸", color: "#9B5BBE", file: "apps/instashan/index.html" },
+  { name: "Diary", icon: "📖", color: "#A08E77", file: "apps/diary/index.html" },
+  { name: "Browser", icon: "🌐", color: "#5D6B9C", file: "apps/browser/index.html" },
+  { name: "Tasks", icon: "📋", color: "#FF6B6B", file: "apps/task/index.html" },
+  { name: "Settings", icon: "⚙️", color: "#555555", file: "apps/settings/index.html" }
 ];
 
 function loadApps() {
+  appGrid.innerHTML = "";
   apps.forEach(app => {
     const a = document.createElement("a");
     a.className = "app-icon";
     a.href = app.file;
+    a.target = "_blank";
+
     a.innerHTML = `
-      <div class="app-icon-body">${app.icon}</div>
+      <div class="app-icon-body" style="background:${app.color}">
+        ${app.icon}
+      </div>
       <div class="app-icon-label">${app.name}</div>
     `;
     appGrid.appendChild(a);
